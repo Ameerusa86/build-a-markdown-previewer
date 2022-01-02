31b6c877ae90f90ac74ff1fd64de642e5f718e84
Elisa Crescentini <elisa@ruggedsoftware.dev>
2022-01-02 17:44:44 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,21 @@
+//Libs
+import { useState } from "react";
+
+//Local
 import Editor from "./Components/Editor";
 import Preview from "./Components/Preview";
+import { placeholder } from "./Components/placeholder";
 
 function Main() {
+  const [value, setValue] = useState(placeholder);
+  function handleChange(input) {
+    setValue(input);
+  }
+
   return (
     <div className="container mx-auto">
-      <Editor />
-      <Preview />
+      <Editor markdown={value} onChange={handleChange} />
+      <Preview markdown={value} />
     </div>
   );
 }
