60f1d4c74ee2c2f11dfb6f5e67a38e67e9fb7067
Elisa Crescentini <elisa@ruggedsoftware.dev>
2022-01-02 17:43:44 +0100

    Update Preview.js

--- src/Components/Preview.js
+++ src/Components/Preview.js
@@ -1,10 +1,22 @@
-export default function Preview() {
+import { marked } from "marked";
+
+export default function Preview({ markdown }) {
+  const renderer = new marked.Renderer();
+  marked.setOptions({
+    breaks: true,
+  });
   return (
-    <div>
-      <div className="bg-teal-300 border-2 border-teal-900 rounded-lg text-6xl text-center">
+    <div className="border-2 border-teal-900 rounded-lg m-3">
+      <div className="bg-teal-700  text-6xl text-center text-white pb-2">
         Preview
       </div>
-      <div id="preview"></div>
+      <div
+        id="preview"
+        className="p-2"
+        dangerouslySetInnerHTML={{
+          __html: marked(markdown, { renderer: renderer }),
+        }}
+      />
     </div>
   );
 }
